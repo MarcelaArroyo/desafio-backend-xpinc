83ae799fdf1cf9dc0749d2340ffeeea3dbd7182c
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-20 18:02:23 -0300

    fix: adicionando a coluna 'versao' no db

--- InvestimentoAcoes.sql
+++ InvestimentoAcoes.sql
@@ -19,10 +19,12 @@ CREATE TABLE `ativos` (
 ) ENGINE=InnoDB;
 
 CREATE TABLE `carteiras` (
+	`codCarteira` integer NOT NULL AUTO_INCREMENT,
 	`codCliente` integer NOT NULL,
     `codAtivo` integer NOT NULL,
     `qtdeAtivo` integer NOT NULL,
-    PRIMARY KEY (`codCliente`, `codAtivo`),
+    `versao` integer NOT NULL,
+    PRIMARY KEY (`codCarteira`),
 	FOREIGN KEY (`codCliente`) REFERENCES `clientes`(`codCliente`),
     FOREIGN KEY (`codAtivo`) REFERENCES `ativos`(`codAtivo`)
 ) ENGINE=InnoDB;
@@ -31,8 +33,22 @@ CREATE TABLE `contasInvestimento` (
 	`codConta` integer NOT NULL AUTO_INCREMENT,
     `codCliente` integer NOT NULL,
     `saldo` decimal(10, 2) NOT NULL,
+    `versao` integer NOT NULL,
     PRIMARY KEY (codConta),
     FOREIGN KEY (codCliente) REFERENCES clientes(codCliente)
+) ENGINE=InnoDB;
+
+CREATE TABLE `historicoTransacao` (
+	`codTransacao` integer NOT NULL AUTO_INCREMENT,
+    `codCliente` integer NOT NULL,
+    `codAtivo` integer NOT NULL,
+    `qtdeAtivo` integer NOT NULL,
+    `tipoTransacao` varchar(50) NOT NULL,
+    `valor` decimal(10, 2) NOT NULL,
+    `data` datetime NOT NULL,
+    PRIMARY KEY (codTransacao),
+    FOREIGN KEY (codCliente) REFERENCES clientes(codCliente),
+    FOREIGN KEY (codAtivo) REFERENCES ativos(codAtivo)
 ) ENGINE=InnoDB;
 
 INSERT INTO `clientes` (`nome`, `email`, `password`) VALUES
@@ -42,19 +58,26 @@ INSERT INTO `clientes` (`nome`, `email`, `password`) VALUES
     ('José Alves', 'josealves@teste.com', '456789');
     
 INSERT INTO `ativos` (`nome`, `valor`, `qtdeAtivo`) VALUES
-	('AZUL4', '350.00', '100'),
-    ('PETR4', '350.00', '100'),
-    ('MGLU3', '350.00', '100'),
-    ('SAPR11', '350.00', '100');
+	('AZUL4', 12.19, 100),
+    ('PETR4', 27.96, 100),
+    ('MGLU3', 2.78, 100),
+    ('SAPR11', 17.77, 100);
     
-INSERT INTO `carteiras` (`codCliente`, `codAtivo`, `qtdeAtivo`) VALUES
-	('1', '4', '50'),
-    ('1', '2', '30'),
-    ('3', '2', '100'),
-    ('4', '1', '200');
+INSERT INTO `carteiras` (`codCliente`, `codAtivo`, `qtdeAtivo`, `versao`) VALUES
+	(1, 4, 50, 1),
+    (1, 2, 30, 1),
+    (3, 2, 100, 1),
+    (4, 1, 200, 1);
     
-INSERT INTO `contasInvestimento` (`codCliente`, `saldo`) VALUES
-	('3', '200.00'),
-    ('2', '100.00'),
-    ('4', '1000.00'),
-    ('1', '300.00');
+INSERT INTO `contasInvestimento` (`codCliente`, `saldo`, `versao`) VALUES
+	(3, 200.00, 1),
+    (2, 100.00, 1),
+    (4, 1000.00, 1),
+    (1, 300.00, 1);
+    
+INSERT INTO `historicoTransacao` (`codCliente`, `codAtivo`, `qtdeAtivo`, `tipoTransacao`, `valor`, `data`)
+VALUES
+	(1, 2, 10, 'compra', 279.60, '2022-07-12 16:54:32'),
+    (1, 3, 30, 'compra', 83.40, '2022-07-12 10:23:22'),
+    (2, 4, 100, 'compra', 1777.00, '2022-07-13 16:54:32'),
+    (1, 3, 20, 'venda', 55.60, '2022-07-14 17:55:12');
