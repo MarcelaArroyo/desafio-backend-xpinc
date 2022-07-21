a11e29f824965682a896dbb3172b1b80c37bb44d
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-21 11:11:54 -0300

    feat: criando o model da rota GET /ativos/cliente/:codCliente

--- src/models/ativos.model.ts
+++ src/models/ativos.model.ts
@@ -1,5 +1,6 @@
 import { RowDataPacket } from "mysql2";
 import IAtivo from "../interfaces/IAtivo.interface";
+import IAtivos from "../interfaces/IAtivos.interface";
 import connection from "./connection";
 
 export const buscarAtivoPeloCodAtivo = async (codAtivo: number):
@@ -11,4 +12,22 @@ Promise<IAtivo[]> => {
   );
 
   return ativo as IAtivo[];
-}
+};
+
+export const buscarCarteiraPeloCodCliente = async (codCliente: number):
+Promise<IAtivos[]> => {
+  const [ativos] = await connection.execute<RowDataPacket[]>(
+    `SELECT cart.codCliente, cart.codAtivo, cart.qtdeAtivo, cli.codCliente, ativos.valor
+    FROM investimentoAcoes.carteiras AS cart
+    INNER JOIN investimentoAcoes.clientes AS cli
+    ON cart.codCliente = cli.codCliente
+    INNER JOIN investimentoAcoes.ativos AS ativos
+    ON cart.codAtivo = ativos.codAtivo
+    WHERE cli.codCliente = ?;`,
+    [codCliente]
+  );
+    
+  return ativos as IAtivos[];
+};
+
+
