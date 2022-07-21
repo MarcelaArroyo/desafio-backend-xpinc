25418530cc42d4df72cfb1d156241413cb599718
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-21 11:11:08 -0300

    feat: criando o controller da rota GET /ativos/cliente/:codCliente

--- src/controllers/ativos.controller.ts
+++ src/controllers/ativos.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { ativoPeloCodAtivo } from '../services/ativos.service';
+import { ativoPeloCodAtivo, ativosPeloCodCliente } from '../services/ativos.service';
 
 export const controllerAtivo = async (req: Request, res: Response): 
 Promise<Response> => {
@@ -8,6 +8,20 @@ Promise<Response> => {
     const result = await ativoPeloCodAtivo(+codAtivo)
 
     if (!result) return res.status(404).json({ message: 'Ativo não encontrado' });
+
+    return res.status(200).json(result);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const controllerAtivoCliente = async (req: Request, res: Response): 
+Promise<Response> => {
+  try {
+    const { codCliente } = req.params;
+    const result = await ativosPeloCodCliente(+codCliente)
+
+    if (!result) return res.status(404).json({ message: 'Cliente não encontrado' });
 
     return res.status(200).json(result);
   } catch (error: any) {
