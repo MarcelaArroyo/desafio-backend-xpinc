ed7db697b6add7a418520eee4d27c0870e92035a
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-20 21:08:04 -0300

    feat: adicionando a rota POST /investimento/vender e seu controller

--- routes/investimentos.routes.ts
+++ routes/investimentos.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { controllerComprar } from '../src/controllers/investimentos.controller';
+import { controllerComprar, controllerVender } from '../src/controllers/investimentos.controller';
 
 const routes = Router();
 
 routes.post('/investimentos/comprar', controllerComprar);
+routes.post('/investimentos/vender', controllerVender);
 
 export default routes;
