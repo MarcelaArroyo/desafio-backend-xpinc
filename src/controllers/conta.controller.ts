305ad16099787ac40ea17567e8830beb81187ee3
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-21 15:11:41 -0300

    feat: criando o controller da rota POST /conta/saque

--- src/controllers/conta.controller.ts
+++ src/controllers/conta.controller.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express';
-import { contaDeposito } from '../services/conta.service';
+import { contaDeposito, contaSaque } from '../services/conta.service';
 
 export const controllerContaDeposito = async (req: Request, res: Response): 
 Promise<Response> => {
   try {
     const { codCliente, valor } = req.body;
     const result = await contaDeposito(codCliente, valor);
+    return res.status(result.status).json({ message: result.message});
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const controllerContaSaque = async (req: Request, res: Response): 
+Promise<Response> => {
+  try {
+    const { codCliente, valor } = req.body;
+    const result = await contaSaque(codCliente, valor);
     return res.status(result.status).json({ message: result.message});
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
