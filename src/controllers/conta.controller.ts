af689ddb7cdc4c16f78f5adfbb21aa7cb8c25518
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-21 15:32:25 -0300

    feat: criando o controller da rota GET /conta/:codCliente

--- src/controllers/conta.controller.ts
+++ src/controllers/conta.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { contaDeposito, contaSaque } from '../services/conta.service';
+import { contaDeposito, contaSaque, contaCliente } from '../services/conta.service';
 
 export const controllerContaDeposito = async (req: Request, res: Response): 
 Promise<Response> => {
@@ -18,6 +18,18 @@ Promise<Response> => {
     const { codCliente, valor } = req.body;
     const result = await contaSaque(codCliente, valor);
     return res.status(result.status).json({ message: result.message});
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const controllerContaCliente = async (req: Request, res: Response): 
+Promise<Response> => {
+  try {
+    const { codCliente } = req.params;
+    const result = await contaCliente(+codCliente);
+    if (!result) return res.status(404).json({ message: 'Conta não encontrada' });
+    return res.status(200).json(result);
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
