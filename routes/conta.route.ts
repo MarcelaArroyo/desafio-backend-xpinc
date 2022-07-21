a1229b7e8139e8f71980a2be551d9e12bd8a1125
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-21 15:31:54 -0300

    feat: criando a rota GET /conta/:codCliente

--- routes/conta.route.ts
+++ routes/conta.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { controllerContaDeposito, controllerContaSaque } from '../src/controllers/conta.controller';
+import { controllerContaDeposito, controllerContaSaque, controllerContaCliente } from '../src/controllers/conta.controller';
 
 const routes = Router();
 
 routes.post('/conta/deposito', controllerContaDeposito);
 routes.post('/conta/saque', controllerContaSaque);
+routes.get('/conta/:codCliente', controllerContaCliente)
 
 export default routes;
