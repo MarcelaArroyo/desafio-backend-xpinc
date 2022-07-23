e8598e2a444dbc26bea8cdc43a59a29cad265cd7
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-23 20:47:08 -0300

    refactor: patronizando os códigos de investimentos

--- routes/investimentos.routes.ts
+++ routes/investimentos.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { controllerComprar, controllerVender } from '../src/controllers/investimentos.controller';
+import investimentosController from '../src/controllers/investimentos.controller';
 import { validacaoToken } from '../src/middlewares/token.middleware';
 import { investimentosValidacao } from '../src/middlewares/investimentos.middleware';
 
 const routes = Router();
 
-routes.post('/investimentos/comprar', validacaoToken, investimentosValidacao, controllerComprar);
-routes.post('/investimentos/vender', validacaoToken, investimentosValidacao, controllerVender);
+routes.post('/investimentos/comprar', validacaoToken, investimentosValidacao, investimentosController.comprarAtivos);
+routes.post('/investimentos/vender', validacaoToken, investimentosValidacao, investimentosController.venderAtivos);
 
 export default routes;
--- src/controllers/investimentos.controller.ts
+++ src/controllers/investimentos.controller.ts
@@ -1,24 +1,29 @@
 import { Request, Response } from 'express';
-import { comprarAtivo, venderAtivo } from "../services/investimentos.service";
+import investimentosService from "../services/investimentos.service";
 
 
-export const controllerComprar = async (req: Request, res: Response): 
+const comprarAtivos = async (req: Request, res: Response): 
 Promise<Response> => {
   try {
-    const result = await comprarAtivo(req.body);
-    return res.status(result.status).json({ message: result.message });
-  } catch (error: any) {
+    const compra = await investimentosService.comprarAtivos(req.body);
+    return res.status(compra.status).json({ message: compra.message });
+  } catch (error: unknown | any) {
     return res.status(500).json({ message: error.message });
-  }
+  };
 };
 
-export const controllerVender = async (req: Request, res: Response): 
+const venderAtivos = async (req: Request, res: Response): 
 Promise<Response> => {
   try {
-    const result = await venderAtivo(req.body);
-    return res.status(result.status).json({ message: result.message });
-  } catch (error: any) {
+    const venda = await investimentosService.venderAtivos(req.body);
+    return res.status(venda.status).json({ message: venda.message });
+  } catch (error: unknown | any) {
     return res.status(500).json({ message: error.message });
-  }
+  };
 };
+
+export default {
+  comprarAtivos,
+  venderAtivos
+}
 
