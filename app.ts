d1d14ea72139345435c1dfe1c0f42822fac3a867
Marcela Arroyo <malvesarroyo@gmail.com>
2022-07-23 20:55:40 -0300

    fix: removendo um middleware de erro

--- app.ts
+++ app.ts
@@ -3,7 +3,6 @@ import investimentosRoutes from './routes/investimentos.routes';
 import ativosRoutes from './routes/ativos.route';
 import contaRoutes from './routes/conta.route';
 import autorizacaoRoutes from './routes/autorizacao.route';
-import httpErroMiddleware from './src/middlewares/http.erro.middleware';
 
 const app = express();
 
@@ -12,6 +11,5 @@ app.use(investimentosRoutes);
 app.use(ativosRoutes);
 app.use(contaRoutes);
 app.use(autorizacaoRoutes);
-app.use(httpErroMiddleware);
 
 export default app;
